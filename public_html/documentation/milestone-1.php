0dcd852741c8f1f2c2d5c32a28ef5ab6c591f80d
Billy Trabaudo <btrabaudo@gmail.com>
2017-04-26 16:53:59 -0600

    Added persona

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Milestone 1</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Milestone 1</h1>
+        </header>
+
+    <main>
+        <h2>Persona</h2>
+
+        <p><strong>David Spector</strong></p>
+
+        <p><strong>Age:</strong>35</p>
+
+        <p><strong>User Story:</strong> David wants to be impressed by a perspective hire.</p>
+
+        <p><strong>Work Life:</strong> David, is senior Pen Tester at a multi-national tech security company. He has a intense background in critical thinking and problem solving. Prior to working for this company he was an employee at the DOD and was a computer science major in college. He is extremely detail oriented. He spends most of his day taking apart websites and finding areas to exploit in code. </p>
+        <p><strong>Personal Life:</strong> Work is life for David. In his little personal time he enjoys utility oriented activities. If they don't serve a clear and distinct purpose he is frustrated or annoyed to the point of disengaging. He does not drink and is very motivated about his personal health. He does from time to time enjoy travel but only to interesting (high-risk) places.</p>
+
+        <p><strong>Professional Needs:</strong> Although his company is looking to hire, they are only looking for the best. Which means, a strong knowledge of tech, coding, and pen testing, but also an interesting employee who comes from a background that highlights problem solving anc creative thinking.</p>
+
+        <p><strong>Tech Profile:</strong> The importance of this section can not be underestimated with David. He looks at code everyday from some of the best coders in the world. His clients include places like Uber and Bank of America. He will be accessing the website via a high-powered laptop.</p>
+
+        <p><strong>Persona Conclusions:</strong> David, wants to get the most of his time and is interested in clear, clean, designs that are beautiful because of their utility and simplicity. It should be noted that David will look almost instantly at the source code and also probe for areas to exploit in a website. Having ugly code or insecure sites will instantly be a red flag.</p>
+
+
+    </main>
+
+
+    </body>
+
+
+</html>
+
