d3933e5c6b5ff214e19ecb84b5ebd69a2a1fef25
Billy Trabaudo <btrabaudo@gmail.com>
2017-05-08 14:31:14 -0600

    added milestone-2

---
+++ public_html/documentation/milestone-2/milestone-2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Milestone 2</title>
+
+    </head>
+
+    <body>
+        <header>
+            <h1>Milestone 2</h1>
+        </header>
+
+    <main>
+        <h2>Content Strategy</h2>
+
+        <p><strong>Primary Objective for Content:</strong> The content of the site should reflect a professional site of a full stack developer and a emerging pen tester. It should feature projects and links to completed sites, in addition it should include links to my github, linkedin, and bugbouty profiles.</p>
+
+        <p><strong>Secondary Objective for Content:</strong>The site should also reflect a certain intrigue about me personally. With information that makes my personal life stand out as an interesting person and potential employee.</p>
+
+    </main>
+    </body>
+
+
+</html>
