13c69e3f94f274197973320ddd4b4797fdcad054
Billy Trabaudo <btrabaudo@gmail.com>
2017-05-30 11:26:25 -0600

    added contact form layout

--- public_html/index.php
+++ public_html/index.php
@@ -77,9 +77,10 @@
                         <img src="../public_html/images/euclid1.gif" alt="Euclid Prop 1.47">
                         <div class="caption">
                             <h3>Personal Life</h3>
-                            <p>Vegan aesthetic occupy leggings art party cardigan. Put a bird on it tumblr blog etsy. Pickled umami ramps unicorn, +1 paleo cloud bread direct trade taiyaki woke pour-over sriracha health goth leggings. Chicharrones occupy aesthetic irony waistcoat, thundercats twee drinking vinegar viral narwhal venmo lo-fi four dollar toast truffaut.</p>
+
                         </div>
                     </a>
+                    <p>Vegan aesthetic occupy leggings art party cardigan. Put a bird on it tumblr blog etsy. Pickled umami ramps unicorn, +1 paleo cloud bread direct trade taiyaki woke pour-over sriracha health goth leggings. Chicharrones occupy aesthetic irony waistcoat, thundercats twee drinking vinegar viral narwhal venmo lo-fi four dollar toast truffaut.</p>
                 </div>
             </div>
 
@@ -98,7 +99,67 @@
     </div>
     </body>
 
+    <!-- Contact Section -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <h3>Contact</h3>
+                <!--Begin Contact Form-->
+                <form id="contact-form" action="php/mailer.php" method="post">
+                    <div class="form-group">
+                        <label for="name">Name <span class="text-danger">*</span></label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </div>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email <span class="text-danger">*</span></label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                            </div>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="subject">Subject</label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <i class="fa fa-pencil" aria-hidden="true"></i>
+                            </div>
+                            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Message <span class="text-danger">*</span></label>
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <i class="fa fa-comment" aria-hidden="true"></i>
+                            </div>
+                            <textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+                        </div>
+                    </div>
+            </div>
 
+            <!-- reCAPTCHA -->
+            <div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+            <button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+            <button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+            </form>
+
+            <!--empty area for form error/success output-->
+            <div class="row">
+                <div class="col-xs-12">
+                    <div id="output-area"></div>
+                </div>
+            </div>
+        </div><!--/.row-->
+        </div><!--/.container-->
+    </section>
 
 
 
